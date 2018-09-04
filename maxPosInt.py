49b2d77526e2bc98bac7bc9b49d94ef19d1ac6a8
JonHS97 <jons18@ru.is>
2018-09-04 11:01:28 +0000

    Finish maxPosInt.py and added sequence.py

--- maxPosInt.py
+++ maxPosInt.py
@@ -1,3 +1,15 @@
-num_int = int(input("input a number: "))
+num_list = []
+while True:
+    num_int = int(input("input a number: "))
+    num_list.append(num_int)
+    if num_int < 0:
+        break
 
+max_int = max(num_list)
+
+
+#get input from user
+#get continuous input from user until he puts a negative int
+#keep track of highest int and print it out when users inputs a negative int
 print("The maximum is", max_int)
+#comment
